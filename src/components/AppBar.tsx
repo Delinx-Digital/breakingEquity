6a3906eb8f586343f02f90a3dff7d4b86b1670a7
Ognyan Marionv <oomarinov@gmail.com>
2021-01-15 16:34:46 +0200

    Adding styles to Menu and AppBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,16 +1,114 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import Logo from './Logo'
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Link, useHistory } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Chip } from '@material-ui/core';
+import { BarChart2, Layers, Zap, User, Menu } from 'react-feather';
+import Logo from './Logo';
+
+const useStyles = makeStyles({
+    headerWrapper: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+    },
+    column: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    logo: {
+        marginLeft: 20,
+        display: 'block',
+    },
+    notifications: {
+        marginRight: 10,
+
+        '& > div': {
+            marginRight: 10,
+
+            '&:last-child': {
+                marginRight: 0,
+            }
+        }
+    }
+});
+
+const Notification = withStyles({
+    root: {
+        backgroundColor: 'transparent',
+    },
+    icon: {
+        marginRight: 0,
+    }
+})(Chip);
+
+const AccountButton = withStyles({
+    root: {
+        backgroundColor: 'transparent',
+    },
+    icon: {
+        marginRight: 0,
+    }
+})(Chip);
+
+const notifications = [
+    {
+        name: 'test',
+        icon: BarChart2,
+        value: 1,
+        to: '/',
+    },
+    {
+        name: 'test',
+        icon: Layers,
+        value: 2,
+        to: '/',
+    },
+    {
+        name: 'test',
+        icon: Zap,
+        value: 3,
+        to: '/',
+    }
+];
 
 const AppBarComponent = ()=> {
+    const classes = useStyles();
+    const history = useHistory();
+
     return (
         <AppBar color="default">
             <Toolbar>
-                <IconButton>
-                    <MenuIcon />
-                </IconButton>
-                <Logo />
+                <div className={classes.headerWrapper}>
+                    <div className={classes.column}>
+                        <IconButton>
+                            <Menu />
+                        </IconButton>
+                        <Link to="/" className={classes.logo}>
+                            <Logo />
+                        </Link>
+                    </div>
+
+                    <div className={classes.column}>
+                        <div className={classes.notifications}>
+                            {notifications.map(({ name, icon: Icon, value, to }, key)=> (
+                                <Notification
+                                    key={`${name}-${key}`}
+                                    size="small"
+                                    label={value}
+                                    icon={<Icon />}
+                                    onClick={()=> history.push(to)}
+                                />
+                            ))}
+                        </div>
+
+                        <AccountButton
+                            size="small"
+                            icon={<User/>}
+                            label="Morgan Smith"
+                            onClick={()=> history.push('/')}
+                        />
+                    </div>
+                </div>
             </Toolbar>
         </AppBar>
     )
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 import { MenuItemLink, getResources } from 'react-admin';
 
+const useStyles = makeStyles({
+    sideBar: {
+        background: '#fff',
+        height: '100%',
+        paddingTop: 40,
+    },
+});
+
 const Menu = ({ onMenuClick }: any) => {
+    const classes = useStyles();
     const {sidebarOpen} = useSelector((state: any) => state.admin.ui);
     const resources = useSelector(getResources);
 
     return (
-        <div style={{ background: '#fff', height: '100%' }}>
+        <div className={classes.sideBar}>
             {resources.map(({ name, options, icon: Icon }: any) => (
                 <MenuItemLink
                     key={name}
