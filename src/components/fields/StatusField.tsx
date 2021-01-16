358b858d7edec6fefed8e5017d52d10798112039
Ognyan Marionv <oomarinov@gmail.com>
2021-01-16 19:02:11 +0200

    Styling status field

--- src/components/fields/StatusField.tsx
+++ src/components/fields/StatusField.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { Chip } from '@material-ui/core'
+import { Chip } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core';
+import { grey, red, lightGreen, green } from '@material-ui/core/colors';
+
+const useStatus = makeStyles({
+    COMPLETED: {
+        background: lightGreen[50],
+        color: green[600],
+    },
+    STOPPED: {
+        background: grey[200],
+        color: grey[600],
+    },
+    FAILED: {
+        background: grey[300],
+        color: red[500],
+    },
+});
+
+const StatusChip = withStyles(()=> ({
+    root: {
+        borderRadius: 0,
+        height: 'auto',
+    },
+    label: {
+        padding: '2px 6px',
+        fontWeight: 600,
+    },
+}))(Chip);
 
 const StatusField = ({source, record}: any) => {
-    const status = record[source];
+    const statusStyle: any = useStatus();
+    const currentStatus = record[source];
+
     return (
-        <Chip label={status} />
+        <StatusChip
+            className={statusStyle?.[currentStatus]}
+            label={currentStatus}
+        />
     )
 };
 
